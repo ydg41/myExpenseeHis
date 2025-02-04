6cca204ab54547a1e94382b210ddcf1e8ea2e6a1
ydg <ydg41@naver.com>
2025-02-04 15:51:49 +0900

    초기

---
+++ src/test/java/com/expenses/history/HistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenses.history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
